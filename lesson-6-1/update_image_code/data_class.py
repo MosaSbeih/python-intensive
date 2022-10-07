f36224649942786ab7a9cf0f52ec4655edd94cbc
MosaSbeih <114357938+MosaSbeih@users.noreply.github.com>
2022-10-07 03:47:43 -0700

    Update lesson-6-1/update_image_code/data_class.py
    Co-authored-by: Dmytro Bolharov <113103592+dbsigma@users.noreply.github.com>

--- lesson-6-1/update_image_code/data_class.py
+++ lesson-6-1/update_image_code/data_class.py
@@ -4,12 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class Point:
 
-    x: int
-    y: int
-
-    @classmethod
-    def default(cls):
-        return Point(x=0, y=0)
+    x: int = 0
+    y: int = 0
 
     def to_tuple(self) -> tuple:
         return self.x, self.y
