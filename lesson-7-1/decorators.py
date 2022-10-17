b993261cdae582d128431dc0a48b9d11a9c7d46e
MosaSbeih <mosasbeih2003@gmail.com>
2022-10-17 22:24:02 +0300

    Update

--- lesson-7-1/decorators.py
+++ lesson-7-1/decorators.py
@@ -3,15 +3,33 @@ import locale
 from functools import wraps
 
 
-def short_form(func) -> list[str]:
+def abbr_data():
+    data_dict = {}
+    weekday_data = list(calendar.day_name)
+    abbr_weekday_date = list(calendar.day_abbr)
+
+    for index in range(len(weekday_data)):
+        data_dict[weekday_data[index]] = abbr_weekday_date[index]
+
+    return data_dict
+
+
+def short_form(func):
     @wraps(func)
-    def wrapper():
-        return list(calendar.day_abbr)
+    def wrapper(*args, **kwargs):
+        data = abbr_data()
+        abbr_weekday_list = []
+        weekdays_list = list(func(*args, **kwargs))
+
+        for day in weekdays_list:
+            abbr_weekday_list.append(data[day])
+
+        return abbr_weekday_list
 
     return wrapper
 
 
-def translate(language: str = "English") -> list[str]:
+def translate(language: str = "English"):
     def inner_function(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
--- lesson-7-1/main.py
+++ lesson-7-1/main.py
@@ -5,8 +5,8 @@ from decorators import short_form, translate
 
 @translate(language="German")
 @short_form
-def get_data() -> list[str]:
-    return list(calendar.day_name)
+def get_data():
+    return calendar.day_name
 
 
 if __name__ == '__main__':
