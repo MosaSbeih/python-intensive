dec1e4f18b9418cacee8cf12423aa5320acfd3d3
MosaSbeih <mosasbeih2003@gmail.com>
2022-11-25 21:32:23 +0200

    Hey Dima I didn't know how to work with creating data classes solutions because the software created a lot of classes.
    but I thought of another solution that takes only a couple lines of code in
    a more efficient way.

--- lesson-10/main.py
+++ lesson-10/main.py
@@ -8,7 +8,7 @@ import urllib.request
 from aiohttp import ClientSession
 
 
-SERVER_URL = "http://localhost:5001/"
+SERVER_URL = "http://localhost:5050/"
 POKEMON_NAME_LIST = ('abra', 'aerodactyl', 'alakazam', 'arbok', 'arcanine', 'articuno', 'beedrill', 'bellsprout', 'blastoise', 'bulbasaur', 'butterfree', 'caterpie', 'chansey', 'charizard', 'charmander', 'charmeleon', 'clefable', 'clefairy', 'cloyster', 'cubone', 'dewgong', 'diglett', 'ditto', 'dodrio', 'doduo', 'dragonair', 'dragonite', 'dratini', 'drowzee', 'dugtrio', 'eevee', 'ekans', 'electabuzz', 'electrode', 'exeggcute', 'exeggutor', 'farfetchd', 'fearow', 'flareon', 'gastly', 'gengar', 'geodude', 'gloom', 'golbat', 'goldeen', 'golduck', 'golem', 'graveler', 'grimer', 'growlithe', 'gyarados', 'haunter', 'hitmonchan', 'hitmonlee', 'horsea', 'hypno', 'ivysaur', 'jigglypuff', 'jolteon', 'jynx', 'kabuto', 'kabutops', 'kadabra', 'kakuna', 'kangaskhan', 'kingler', 'koffing', 'krabby', 'lapras', 'lickitung', 'machamp', 'machoke', 'machop', 'magikarp', 'magmar', 'magnemite', 'magneton', 'mankey', 'marowak', 'meowth', 'metapod', 'mewtwo', 'moltres', 'mr-mime', 'muk', 'nidoking', 'nidoqueen', 'nidoran-f', 'nidoran-m', 'nidorina', 'nidorino', 'ninetales', 'oddish', 'omanyte', 'omastar', 'onix', 'paras', 'parasect', 'persian', 'pidgeot', 'pidgeotto', 'pidgey', 'pikachu', 'pinsir', 'poliwag', 'poliwhirl', 'poliwrath', 'ponyta', 'porygon', 'primeape', 'psyduck', 'raichu', 'rapidash', 'raticate', 'rattata', 'rhydon', 'rhyhorn', 'sandshrew', 'sandslash', 'scyther', 'seadra', 'seaking', 'seel', 'shellder', 'slowbro', 'slowpoke', 'snorlax', 'spearow', 'squirtle', 'starmie', 'staryu', 'tangela', 'tauros', 'tentacool', 'tentacruel', 'vaporeon', 'venomoth', 'venonat', 'venusaur', 'victreebel', 'vileplume', 'voltorb', 'vulpix', 'wartortle', 'weedle', 'weepinbell', 'weezing', 'wigglytuff', 'zapdos', 'zubat')
 
 
@@ -17,7 +17,7 @@ def random_pokemon() -> str:
     return random.choice(POKEMON_NAME_LIST)
 
 
-async def download_image(img_url: str, directory) -> None:
+def download_image(img_url: str, directory) -> None:
 
     urllib.request.urlretrieve(img_url, directory)
 
@@ -33,6 +33,15 @@ async def get_pokemon_json(session: ClientSession, url: str) -> dict:
 
 async def main(num: int) -> None:
 
+    def loop(json_data: dict, file_name: str, directory: str) -> None:
+
+        for key in json_data.keys():
+            if isinstance(json_data[key], str):
+                download_image(json_data[key], os.path.join(directory, file_name + f"{key}.jpg"))
+            elif isinstance(json_data[key], dict):
+                new_file_name = file_name + f"-{key}"
+                loop(json_data[key], new_file_name, directory)
+
     os.mkdir("pokemon_images")
 
     async with ClientSession() as session:
@@ -44,8 +53,6 @@ async def main(num: int) -> None:
 
         pokemons = await asyncio.gather(*pokemons)
 
-        tasks = []
-
         for pokemon in pokemons:
             pokemon_image_dir = os.path.join("pokemon_images", pokemon["name"])
             try:
@@ -53,31 +60,7 @@ async def main(num: int) -> None:
             except FileExistsError:
                 pass
 
-            for key in pokemon["sprites"].keys():
-                url = pokemon["sprites"][key]
-
-                if isinstance(url, str):
-                    tasks.append(asyncio.ensure_future(
-                        download_image(url, os.path.join(pokemon_image_dir, f"{key}.jpg"))))
-
-                # Accessing dictionaries
-                elif isinstance(url, dict):
-                    for key_1 in url.keys():
-                        for key_2 in url[key_1].keys():
-                            if isinstance(url[key_1][key_2], dict):
-                                for key_3 in url[key_1][key_2].keys():
-                                    if isinstance(url[key_1][key_2][key_3], str):
-                                        tasks.append(asyncio.ensure_future(
-                                            download_image(url[key_1][key_2][key_3],
-                                                           os.path.join(pokemon_image_dir,
-                                                                        f"{key}-{key_1}-{key_2}-{key_3}.jpg"))))
-                            elif isinstance(url[key_1][key_2], str):
-                                tasks.append(asyncio.ensure_future(
-                                    download_image(url[key_1][key_2],
-                                                   os.path.join(pokemon_image_dir,
-                                                                f"{key}-{key_1}-{key_2}.jpg"))))
-
-        await asyncio.gather(*tasks)
+            loop(pokemon["sprites"], "", pokemon_image_dir)
 
 
 if __name__ == "__main__":
